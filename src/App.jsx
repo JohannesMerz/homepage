6d566f9506d523d568dd48fa95e84e1e34706d75
Johannes Merz <JohannesMerz@users.noreply.github.com>
2024-03-19 22:58:21 +0100

    rearrange app routes (#4)

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import {
   RouterProvider,
   createRoutesFromElements,
   Route,
+  Navigate,
 } from 'react-router-dom';
 import { ErrorPage } from './components/ErrorPage';
+import { homeRoutes } from './apps/home/routes';
+import { intervalTimerRoutes } from './apps/interval-timer/routes';
 
 const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" errorElement={<ErrorPage />}>
-      <Route path="/" lazy={() => import('./apps/home')} />
-      <Route
-        path="/interval-timer"
-        lazy={() => import('./apps/interval-timer')}
-      />
+      <Route path="home">{homeRoutes}</Route>
+      <Route path="interval-timer">{intervalTimerRoutes}</Route>
+      <Route index element={<Navigate to="/home" replace />} />
     </Route>
   )
 );
---
+++ src/apps/interval-timer/routes.jsx
@@ -0,0 +1,5 @@
+import { Route } from 'react-router-dom';
+
+export const intervalTimerRoutes = (
+  <Route index lazy={() => import('./index')} />
+);

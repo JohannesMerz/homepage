056d55a9e7e47ad79de46a829f577a3fc26edc28
JohannesMerz <johannes@bojagi.io>
2024-03-24 16:12:02 +0100

    setting section scaffold

--- src/apps/interval-timer/components/store-consumers/Settings.jsx
+++ src/apps/interval-timer/components/store-consumers/Settings.jsx
@@ -1,16 +1,52 @@
 import { useWorkoutStore } from '../../model';
 import { Button } from '../atomics/Button';
-import { FiSettings } from 'react-icons/fi';
+import { FiSettings, FiX } from 'react-icons/fi';
 import { VARIANTS } from '../../Theme';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const SettingsInput = styled.div`
+  position: fixed;
+  top: 70px;
+  left: 0;
+  width: 100%;
+  height: 80%;
+  background-color: ${VARIANTS.start.bgColor};
+  border: 2px solid ${VARIANTS.start.color};
+  border-radius: 6px;
+  box-shadow: 0px 0px 4px ${VARIANTS.start.color};
+  z-index: 1000;
+`;
 
 export function Settings() {
   const workoutStore = useWorkoutStore();
 
+  const [settingsOpen, setSettingsOpen] = useState(false);
+
   const color = VARIANTS[workoutStore.phase.name].color;
 
   return (
-    <Button color={color} onClick={console.log}>
-      <FiSettings size="28"></FiSettings>
-    </Button>
+    <>
+      <Button
+        color={color}
+        disabled={
+          settingsOpen ||
+          workoutStore.workout.active ||
+          (workoutStore.workout.progressMs && workoutStore.phase.name !== 'end')
+        }
+        onClick={() => setSettingsOpen(true)}
+      >
+        <FiSettings size="28"></FiSettings>
+      </Button>
+      {settingsOpen && (
+        <SettingsInput>
+          <p>this will be the settings section</p>
+          <p>{JSON.stringify(workoutStore.settings, null, 2)}</p>
+          <Button color={color} onClick={() => setSettingsOpen(false)}>
+            <FiX size="28"></FiX>
+          </Button>
+        </SettingsInput>
+      )}
+    </>
   );
 }
